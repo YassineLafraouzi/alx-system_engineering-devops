e9b60be69171ce77d3c3b0996a0e7f1f29973b04
Yassine Lafraouzi <mhtajek.sup@gmail.com*>
2024-03-30 14:36:36 -0700

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,5 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'python3-pip':
-  ensure   => installed,
-}
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip'
-  require  => Package['python3-pip'],
-}
-package {'werkzeug':
-  ensure   => '2.0.1',
-  provider => 'pip'
-  require  => Package['python3-pip'],
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
