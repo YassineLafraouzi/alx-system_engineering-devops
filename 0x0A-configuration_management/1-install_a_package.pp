dc9fcae3a11a7a900ce8326d00443eca4ad7db7a
Yassine Lafraouzi <mhtajek.sup@gmail.com*>
2024-03-30 14:34:35 -0700

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
+package {'python3-pip':
+  ensure   => installed,
+}
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip'
+  require  => Package['python3-pip'],
+}
+package {'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip'
+  require  => Package['python3-pip'],
 }
