7ae47f0fffc04ea523cf964776f487cd3c5bf639
Yassine Lafraouzi <mhtajek.sup@gmail.com*>
2024-03-31 07:55:32 -0700

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-# Ensure python3-pip is installed
-package { 'python3-pip':
-  ensure => 'installed',
-}
-
-# Install Flask version 2.1.0 using pip3
+# Install flask
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+  provider => 'pip3'
 }
